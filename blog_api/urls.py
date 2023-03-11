98c506a2cc181f73685c3464da346ff4dea10d56
Mounir <polomnr1@gmail.com>
2023-03-11 09:15:30 +0100

    init the repo and create a django project

---
+++ blog_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import PostList, PostDetail
+
+app_name = 'blog_api'
+
+urlpatterns = [
+    # path('', PostList.as_view(), name='listcreate'),
+    # path('<int:pk>/', PostDetail.as_view(), name='detailcreate')
+]
