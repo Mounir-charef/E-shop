f13ff9d2d7692ac8e51eedd87d22ce0864dba8c5
Mounir <polomnr1@gmail.com>
2023-04-15 00:33:34 +0100

    vercel deployment

--- DRF/settings.py
+++ DRF/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} if DEBUG else {}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
