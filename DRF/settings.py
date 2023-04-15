ff3e62983986f742f0dfe06707295b543d48cd8c
Mounir <polomnr1@gmail.com>
2023-04-15 01:14:02 +0100

    collect static

--- DRF/settings.py
+++ DRF/settings.py
@@ -136,6 +136,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'Frontend-react/dist/assets/'
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
