0f451c9ca5374fbe3f3d9857f32b69f7b3617c6f
Mounir <polomnr1@gmail.com>
2023-05-31 17:59:45 +0100

    footer simple footer

--- Frontend-react/src/components/Footer.jsx
+++ Frontend-react/src/components/Footer.jsx
@@ -1,6 +1,25 @@
+import github_icon from '../assets/github-mark-white.png'
+import instagram_icon from '../assets/icon-instagram.svg'
+import facebook_icon from '../assets/icon-facebook.svg'
+import linkedin_icon from '../assets/icons8-linkedin.svg'
 const Footer = () => {
     return (
-        <div>
+        <div className='flex gap-4 flex-col flex-1 justify-center items-center w-full h-28 bg-slate-700'>
+            <p className='text-white text-sm'>Copyright &copy; {new Date().getFullYear()}, All Rights Mounir Charef</p>
+            <div className="flex justify-center gap-3">
+                <a href="https://www.facebook.com/Chrf.Mounir" target='_blank'>
+                    <img src={facebook_icon} alt="fb" className="h-6 hover:opacity-75 transition"/>
+                </a>
+                <a href="https://www.instagram.com/chrf_mounir/" target='_blank'>
+                    <img src={instagram_icon} alt="fb" className="h-6 hover:opacity-75 transition"/>
+                </a>
+                <a href="https://github.com/lolifmaster" target='_blank'>
+                    <img src={github_icon} alt="fb" className="h-6 hover:opacity-75 transition"/>
+                </a>
+                <a href="https://www.linkedin.com/in/mounir-charef-3397b1229/" target='_blank'>
+                    <img src={linkedin_icon} alt="fb" className="h-7 hover:opacity-75 transition"/>
+                </a>
+            </div>
 
         </div>
     );
